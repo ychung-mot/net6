10248b9e804d35a96f209b570306f1e61847bd13
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-11-18 07:48:21 -0800

    Fix - Obsolete RNGCryptoServiceProvider

--- api/Crt.Domain/Services/KeyCloakService.cs
+++ api/Crt.Domain/Services/KeyCloakService.cs
@@ -225,41 +225,40 @@ namespace Crt.Domain.Services
         // https://blog.bitscry.com/2018/04/13/cryptographically-secure-random-string/
         private string GetUniqueToken(int length, string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
         {
-            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            using var crypto = RandomNumberGenerator.Create();
+
+            byte[] data = new byte[length];
+
+            // If chars.Length isn't a power of 2 then there is a bias if we simply use the modulus operator. The first characters of chars will be more probable than the last ones.
+            // buffer used if we encounter an unusable random byte. We will regenerate it in this buffer
+            byte[] buffer = null;
+
+            // Maximum random number that can be used without introducing a bias
+            int maxRandom = byte.MaxValue - ((byte.MaxValue + 1) % chars.Length);
+
+            crypto.GetBytes(data);
+
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
             {
-                byte[] data = new byte[length];
+                byte value = data[i];
 
-                // If chars.Length isn't a power of 2 then there is a bias if we simply use the modulus operator. The first characters of chars will be more probable than the last ones.
-                // buffer used if we encounter an unusable random byte. We will regenerate it in this buffer
-                byte[] buffer = null;
-
-                // Maximum random number that can be used without introducing a bias
-                int maxRandom = byte.MaxValue - ((byte.MaxValue + 1) % chars.Length);
-
-                crypto.GetBytes(data);
-
-                char[] result = new char[length];
-
-                for (int i = 0; i < length; i++)
+                while (value > maxRandom)
                 {
-                    byte value = data[i];
-
-                    while (value > maxRandom)
+                    if (buffer == null)
                     {
-                        if (buffer == null)
-                        {
-                            buffer = new byte[1];
-                        }
-
-                        crypto.GetBytes(buffer);
-                        value = buffer[0];
+                        buffer = new byte[1];
                     }
 
-                    result[i] = chars[value % chars.Length];
+                    crypto.GetBytes(buffer);
+                    value = buffer[0];
                 }
 
-                return $"{Constants.AppName}.{new string(result)}";
+                result[i] = chars[value % chars.Length];
             }
+
+            return $"{Constants.AppName}.{new string(result)}";
         }
     }
 }
